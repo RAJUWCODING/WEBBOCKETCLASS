7bb6118c0c9f4a3259330314f0bc2deb13d4f7be
RAHUWCODING <rajuwcoding@gmail.com>
2025-08-01 11:42:54 +0530

    JavaScript  Learning

---
+++ JS/Class 1/script.js
@@ -0,0 +1,32 @@
+console.log("Hello World");
+
+// ! Variable Keyword
+//? var
+var a = 10;
+// console.log(a);
+
+
+var a 
+a= 30;
+// console.log(a);
+
+//? let
+let b = "Hii";
+// console.log(b);
+
+b= 10;
+// console.log(b);
+
+// ? const
+// const c = "Hello";
+//cost c //not allowed
+// c = 20;
+let d 
+d = 20;
+// const e = 20;
+// console.log(d);
+
+
+
+
+
