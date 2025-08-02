d1bbe0c02e327dc2a215971a579c7896f17801e1
RAHUWCODING <rajuwcoding@gmail.com>
2025-08-02 16:59:53 +0530

    Variable Keywoeds Learning

--- JS/Class 1/script.js
+++ JS/Class 1/script.js
@@ -1,32 +1,29 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 // ! Variable Keyword
 //? var
-var a = 10;
+// var a = 10;
 // console.log(a);
 
 
-var a 
-a= 30;
+// var a 
+// a= 30;
 // console.log(a);
 
 //? let
-let b = "Hii";
+// let b = "Hii";
 // console.log(b);
 
-b= 10;
+// b= 10;
 // console.log(b);
 
 // ? const
 // const c = "Hello";
 //cost c //not allowed
 // c = 20;
-let d 
-d = 20;
+// let d 
+// d = 20;
 // const e = 20;
 // console.log(d);
-
-
-
 
 
